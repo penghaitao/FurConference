75a403f9ed1ca5fb08efbd452629e750159f0b49
penghaitao <penghaitao19860226@gmail.com>
2015-12-09 15:16:00 +0000

    add a toast

--- app/src/main/java/com/wartechwick/conference/MainActivity.java
+++ app/src/main/java/com/wartechwick/conference/MainActivity.java
@@ -5,10 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        Toast.makeText(this, "haha", Toast.LENGTH_SHORT).show();
     }
 
     @Override
